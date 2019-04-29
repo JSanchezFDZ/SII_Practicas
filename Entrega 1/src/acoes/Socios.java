72118e6c5bec50ebec52666f574ed1e5fa3cbc21
JSanchez <jsanchezfdz95@gmail.com>
2019-04-29 20:47:42 +0200

    Proyecto ordenado
    Separado el proyecto en Entrega 1 y Entrega 2 para empezar a poder trabajar con la "Entrega 2" en un proyecto diferente.

---
+++ Entrega 1/src/acoes/Socios.java
@@ -0,0 +1,221 @@
+/*
+ * Sistemas de Información para Intenet
+ * Grupo G
+ * Universidad de Málaga
+ */
+package acoes;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Jesús
+ */
+@Entity
+public class Socios implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String estado;
+    private String direccion;
+    private String poblacion;
+    private int codPostal;
+    private String provincia;
+    private String telefono;
+    private String correo;
+    private String sector;
+    private String certificado;
+    @Temporal(TemporalType.DATE)
+    private Date FechaAlta;
+    @Temporal(TemporalType.DATE)
+    private Date FechaBaja;
+    private String observaciones;
+    private List<Apadrinamiento> apadrinamiento; //relacion 1 a muchos con apadrinamiento
+    private Usuario usuario;
+    private Cuota cuota;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public int getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(int codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public Date getFechaAlta() {
+        return FechaAlta;
+    }
+
+    public void setFechaAlta(Date FechaAlta) {
+        this.FechaAlta = FechaAlta;
+    }
+
+    public Date getFechaBaja() {
+        return FechaBaja;
+    }
+
+    public void setFechaBaja(Date FechaBaja) {
+        this.FechaBaja = FechaBaja;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public List<Apadrinamiento> getApadrinamiento() {
+        return apadrinamiento;
+    }
+
+    public void setApadrinamiento(List<Apadrinamiento> apadrinamiento) {
+        this.apadrinamiento = apadrinamiento;
+    }
+
+    public Usuario getUser() {
+        return usuario;
+    }
+
+    public void setUser(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Cuota getCuota() {
+        return cuota;
+    }
+
+    public void setCuota(Cuota cuota) {
+        this.cuota = cuota;
+    }
+
+    public String getCertificado() {
+        return certificado;
+    }
+
+    public void setCertificado(String certificado) {
+        this.certificado = certificado;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Socios)) {
+            return false;
+        }
+        Socios other = (Socios) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "acoes.Socios[ id=" + id + " ]";
+    }
+    
+}

265413990887ecc589fd4ba88a77343e79aac90e
JSanchez <jsanchezfdz95@gmail.com>
2019-05-01 18:49:47 +0200

    Actualización
    Actualización plantilla y "ListaSocios"

--- Entrega 2/src/java/jsf/ListaSocios.java
+++ Entrega 2/src/java/jsf/ListaSocios.java
@@ -5,10 +5,64 @@
  */
 package jsf;
 
+import acoes.Ninos;
+import acoes.Socios;
+import acoes.Usuario;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
 /**
  *
- * @author Sergio
+ * @author Jesús Sánchez Fernández
  */
-public class ListaSocios {
+@Named(value = "ListaSocios")
+@SessionScoped
+public class ListaSocios implements Serializable {
+    private Socios[] socios;
+    private Usuario usuario;
+    
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }  
+    
+    public Socios[] getSocios(){
+        return socios;
+    }
+    
+    public String home() {
+        
+        // Si no ha iniciado sesion, le lleva al login
+        if(getUsuario()==null){
+            return "login.xhtml";
+        }
+        
+        // Si el usuario es un ADMINISTRADOR, le lleva a la pagina a la lista de Socios
+        if(getUsuario().getRol().equals(getUsuario().getRol().ADMINISTRADOR)){
+            return "ListaSocios.xhtml";
+        }
+        
+        // Si el usuario es SOCIO, le llevará a la página web de INDEX
+        // REVISAR
+        if(getUsuario().getRol().equals(getUsuario().getRol().SOCIO)){
+            return "login.xhtml";
+        }
+        
+        return null;
+    }
+    
+    public String logout()
+    {
+        // Destruye la sesión (y con ello, el ámbito de este bean)
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ctx.getExternalContext().invalidateSession();
+        usuario = null;
+        return "login.xhtml";
+    }
     
 }

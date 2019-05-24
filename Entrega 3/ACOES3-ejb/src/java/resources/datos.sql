480aa0d8d6ac21bc4ba72b06ceec44457c82663c
JSanchez <jsanchezfdz95@gmail.com>
2019-05-24 13:29:59 +0200

    Script + Persistence.xml OK
    Ya no se borra al hacer "Clean and Build". Testeado y funcionando.

---
+++ Entrega 3/ACOES3-ejb/src/java/resources/datos.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Jesus Sanchez
+ * Created: 24-may-2019
+ */
+
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, EMAIL, PASS, ROL, SOCIO_ID) VALUES ('admin', 'apellido1', 'admin@acoes.es', 'pass', 0, NULL)
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, EMAIL, PASS, ROL, SOCIO_ID) VALUES ('socio', 'apellido2', 'socio@acoes.es', 'pass', 1, NULL)

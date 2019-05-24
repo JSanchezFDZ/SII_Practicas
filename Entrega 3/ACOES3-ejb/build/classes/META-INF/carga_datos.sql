0408a0294e28acf86908fd0e043772ca72ab9c32
JSanchez <jsanchezfdz95@gmail.com>
2019-05-24 12:53:08 +0200

    Script SQL

---
+++ Entrega 3/ACOES3-ejb/build/classes/META-INF/carga_datos.sql
@@ -0,0 +1,3 @@
+﻿
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, EMAIL, PASS, ROL, SOCIO_ID) VALUES ('admin', 'apellido1', 'admin@acoes.es', 'pass', 0, NULL)
+INSERT INTO APP.USUARIO (USUARIO, APELLIDOS, EMAIL, PASS, ROL, SOCIO_ID) VALUES ('socio', 'apellido2', 'socio@acoes.es', 'pass', 1, NULL)

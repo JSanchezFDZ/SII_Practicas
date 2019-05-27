924854954e11675da9016dcc748de3ebe5915d79
Sergio <Sergio@HP-TURCO>
2019-05-27 12:25:26 +0200

    Revisión ListaSocios

--- Entrega 3/ACOES3-war/src/java/backingbeans/ListaSocios.java
+++ Entrega 3/ACOES3-war/src/java/backingbeans/ListaSocios.java
@@ -6,11 +6,13 @@
 package backingbeans;
 
 import entidades.*;
+import java.awt.event.ActionEvent;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.component.UIComponent;
 import modelos.SociosFacade;
 
 /**
@@ -23,12 +25,18 @@ public class ListaSocios implements Serializable {
     @EJB
     private SociosFacade sociosFacade;
     private Socios s = new Socios();
+    private Long auxId;
 
     
     /*Constructor vacio*/
     public ListaSocios(){
-
+        s.setId((long)111111);
     }
+    
+    public void getId(Long i){
+        auxId = i;
+    }
+
     
     public List<Socios> findAll(){
         return this.sociosFacade.findAll();
@@ -44,6 +52,11 @@ public class ListaSocios implements Serializable {
     
     public String crearSocio(){
         return "insertarSocio.xhtml";
+    }
+    
+    
+    public Long getId(){
+        return s.getId();
     }
     
     public String add(){

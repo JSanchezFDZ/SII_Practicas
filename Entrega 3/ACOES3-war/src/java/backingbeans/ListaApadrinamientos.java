e4cfe696e6b63402177dcaa2331ad47bcc10ff12
jaime <jaime@LAPTOP-R019F3BJ>
2019-05-28 11:22:01 +0200

    formulario socioJMS2.4.2

--- Entrega 3/ACOES3-war/src/java/backingbeans/ListaApadrinamientos.java
+++ Entrega 3/ACOES3-war/src/java/backingbeans/ListaApadrinamientos.java
@@ -48,6 +48,7 @@ public class ListaApadrinamientos implements Serializable {
     }
     
     public String add(){
+        
         this.apadrinamientoFacade.create(this.s);
         this.s = new Apadrinamiento();
         return "listaninosapadrinados.xhtml";
